f585d7072ea39bc0a4b38edfbe5e63c1159224b5
payette-henry <henry@tsok.tech>
2020-03-23 17:22:50 -0600

    successful switch

--- JukeboxHero/src/JukeboxHero.java
+++ JukeboxHero/src/JukeboxHero.java
@@ -6,24 +6,73 @@ public class JukeboxHero
 	{
 		Scanner scan = new Scanner(System.in);
 		String command = new String();
-		command = null;
-		System.out.println("*****************************\r\n" + 
+		String menu = "*****************************\r\n" + 
 				"*      Program Menu         *\r\n" + 
 				"*****************************\r\n" + 
 				"(L)oad catalog\r\n" + 
 				"(S)earch catalog\r\n" + 
 				"(A)nalyse catalog\r\n" + 
 				"(P)rint catalog\r\n" + 
-				"(Q)uit\r\n" + 
-				"\r\n"
-				);
-		while(command == null)
-		{
-			System.out.println("Please enter a command (press 'm' for Menu):");
-			command = scan.nextLine();
-		}
+				"(Q)uit\r\n"
+				;
+		System.out.println(menu);
 
-		System.out.println(command);
+		
+
+		
+		
+		while(!command.equals("quit"))
+		{
+			System.out.println("\r\n"+"Please enter a command (press 'm' for Menu):");
+			
+			command = scan.nextLine();
+
+			switch (command)
+			{
+			case "q":
+			case "Q":
+			case "quit":
+			case "Quit":
+				command = "quit";
+				System.out.println("Goodbye!");
+				break;
+			case "m":
+			case "M":
+			case "menu":
+			case "Menu":
+				System.out.println(menu);
+				break;
+			case "l":
+			case "L":
+			case "load":
+			case "Load":
+				System.out.println("test:load");
+				break;
+			case "s":
+			case "S":
+			case "search":
+			case "Search":
+				System.out.println("test:search");
+				break;
+			case "a":
+			case "A":
+			case "analyze":
+			case "Analyze":
+				System.out.println("test:analyze");
+				break;
+			case "p":
+			case "P":
+			case "print":
+			case "Print":
+				System.out.println("test:print");
+				break;
+			default:
+				System.out.println("Invalid selection!");
+			}
+			//System.out.println("Test:escaped switch!");
+		}
+				
+		//System.out.println("Test:escaped while!");
 		scan.close();
 	}
 }
